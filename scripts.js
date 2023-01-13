94cf7503116c307d747a39ead26cb228adc34d3f
Matheus1221 <58350861+Matheus1221@users.noreply.github.com>
2023-01-13 14:11:27 -0300

    request com axios

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
 // 1 -get fetch
 
 fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((resp) => resp.json())
+  .then((resp) => resp.json()) //resposta recebida e repassando ela em json
   .then((data) => {
-    console.log(data);
+    console.log(data); //pegar a data para utilizalos novamente
+  })
+  .catch((error) => {
+    console.log(error); // aqui pegamos um erro e retornamos ele via console console
   });
 
 // 2 - get Axios
+
+axios
+  .get("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// 3- POST fetch
